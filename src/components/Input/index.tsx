1c94acdc59ab8c383472f53fc0bd1ac9d0641e42
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-07 00:08:29 -0300

    Registrando ref de Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
 import { Container, TextInput, Icon } from './styles';
 
@@ -8,17 +9,37 @@ interface InputProps extends TextInputProps {
   icon: string;
 }
 
-const Button: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+interface InputValueReference {
+  value: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+  const { defaultValue = '', error, registerField, fieldName } = useField(name);
+
+  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+    });
+  }, [inputValueRef, fieldName]);
+
   return (
     <Container>
       <Icon name={icon} size={20} color="#666360" />
       <TextInput
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
+        defaultValue={defaultValue}
+        onChangeText={(value) => {
+          inputValueRef.current.value = value;
+        }}
         {...rest}
       />
     </Container>
   );
 };
 
-export default Button;
+export default Input;
