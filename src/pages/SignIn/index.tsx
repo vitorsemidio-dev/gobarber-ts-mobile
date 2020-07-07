d749d64c3116c20ef846be7d8399b9f2ddaeb005
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-06 23:15:34 -0300

    Estilo para CreateAccountButton

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,36 +1,55 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title, ForgotPassword, ForgotPasswordText } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <Container>
+        <Image source={logoImg} />
 
-      <Title> Faça seu logon</Title>
+        <Title> Faça seu logon</Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="email" icon="mail" placeholder="E-mail" />
 
-      <Input name="password" icon="lock" placeholder="Senha" />
+        <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button
+        <Button
+          onPress={() => {
+            console.log('hahaha');
+          }}
+        >
+          Entrar
+        </Button>
+
+        <ForgotPassword>
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+
+      <CreateAccountButton
         onPress={() => {
           console.log('hahaha');
         }}
       >
-        Entrar
-      </Button>
-
-      <ForgotPassword>
-        <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-      </ForgotPassword>
-    </Container>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar minha conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -23,3 +23,28 @@ export const ForgotPasswordText = styled.Text`
   color: #f4ede8;
   font-family: 'RobotoSlab-Regular';
 `;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  padding: 16px 0;
+
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #ff9000;
+  font-size: 18px;
+  font-family: 'RobotoSlab-Regular';
+
+  margin-left: 16px;
+`;
