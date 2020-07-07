925a4bd41f3e46b08853af8ea158f4abcb2fa54e
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-07 13:13:11 -0300

    Criação AuthRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
 
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import AuthRoutes from './auth.routes';
 
-const Auth = createStackNavigator();
-
-const AuthRoutes: React.FC = () => {
-  return (
-    <Auth.Navigator
-      screenOptions={{
-        headerShown: false,
-        cardStyle: { backgroundColor: '#312e38' },
-      }}
-      initialRouteName="SignIn"
-    >
-      <Auth.Screen name="SignIn" component={SignIn} />
-      <Auth.Screen name="SignUp" component={SignUp} />
-    </Auth.Navigator>
-  );
+const Routes: React.FC = () => {
+  return <AuthRoutes />;
 };
 
-export default AuthRoutes;
+export default Routes;
