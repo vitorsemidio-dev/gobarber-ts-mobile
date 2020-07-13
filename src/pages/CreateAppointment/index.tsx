815347631e6b49b6dbdd77e7b7713f0a4e56acc6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-13 17:14:35 -0300

    Estrutura inicial tela criar agendamento

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  provider_id: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const route = useRoute();
+  const { user } = useAuth();
+
+  const { provider_id } = route.params as RouteParams;
+
+  return (
+    <Container>
+      <Header>
+        <BackButton>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
---
+++ src/pages/CreateAppointment/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View``;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text``;
+
+export const UserAvatar = styled.Image``;
