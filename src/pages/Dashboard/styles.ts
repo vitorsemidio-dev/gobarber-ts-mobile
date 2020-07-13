b7ac944e56bc1ee70da9d03f69d183280a5f908d
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-13 13:42:45 -0300

    Estilo Header Dashboard

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,9 +1,26 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
 
-export const Header = styled.View``;
+export const Header = styled.View`
+  padding: 24px;
+  background: #28262e;
 
-export const HeaderTitle = styled.Text``;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-export const UserName = styled.Text``;
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 24px;
+  font-family: 'RobotoSlab-Regular';
+  line-height: 28px;
+`;
+
+export const UserName = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
