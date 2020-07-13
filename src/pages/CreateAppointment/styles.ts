bf016dfe85d53a8e0be5ac7029e64f249a046599
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-13 17:18:52 -0300

    Estilos header createAppointment

--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,13 +1,37 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
 `;
 
-export const Header = styled.View``;
+export const Header = styled.View`
+  padding: 24px;
+  ${Platform.OS === 'ios' &&
+  css`
+    padding-top: ${getStatusBarHeight() + 24}px;
+  `}
+
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const BackButton = styled.TouchableOpacity``;
 
-export const HeaderTitle = styled.Text``;
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Mediu';
+  font-size: 20px;
+  margin-left: 16px;
+`;
 
-export const UserAvatar = styled.Image``;
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  margin-left: auto;
+`;
