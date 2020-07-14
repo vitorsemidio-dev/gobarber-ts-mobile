1c00a9dc5882b4b4ea674773f804c1fa3f68f6a5
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-14 14:56:57 -0300

    Estilo Tela AppointmentCreated

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04f361" />
+
+      <Title>Agendamento Concluído</Title>
+      <Description>cccc, dia dd de MMMM de yyyy às HH:00</Description>
+
+      <OkButton onPress={() => {}}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
