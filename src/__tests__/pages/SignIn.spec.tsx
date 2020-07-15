096709f80d807d8b35e6efb607aef180a34064a3
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-15 15:01:43 -0300

    [Test]: SignIn renderização da tela

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -3,6 +3,12 @@ import { render } from 'react-native-testing-library';
 
 import SignIn from '../../pages/SignIn';
 
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
+
 describe('SignIn page', () => {
   it('should contains email/password inputs', () => {
     const { getByPlaceholder } = render(<SignIn />);
